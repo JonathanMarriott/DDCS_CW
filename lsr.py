46a63ecb3bca13c053df958a0f109a8462b39896
Jonathan Marriott <34217286+JonathanMarriott@users.noreply.github.com>
2021-03-18 18:44:48 +0000

    Regression for linear, poly, trig

--- lsr.py
+++ lsr.py
@@ -33,9 +33,81 @@ def view_data_segments(xs, ys):
     plt.show()
 
 xs,ys = load_points_from_file(sys.argv[1])
+
+
+
+xs = np.array(xs)
+ys = np.array(ys)
+#print(x,y)
+
+
+def mleFit(x,y):
+    return np.linalg.inv(x.T.dot(x)).dot(x.T).dot(y)
+
+def addBias(x):
+    return np.column_stack((np.ones(x.shape),x))
+
+def addPolyTerms(x,n):
+    xs = addBias(x)
+    for i in range(2,n):
+        xs = np.column_stack((xs,x**(i)))
+    return xs
+
+
+def addTrigTerms(x):
+    return np.column_stack((np.ones(x.shape),np.sin(x),np.cos(x)))
+
+def fitTrig(xs,ys):
+    return mleFit(addTrigTerms(xs),ys)
+
+def fitLinear(xs,ys):
+    return mleFit(addBias(xs),ys)
+
+def fitPoly(xs,ys,n):
+    return mleFit(addPolyTerms(xs,n),ys)
+
+def calcLinearError(xs,ys):
+    numSegs = len(xs) // 20
+    error = 0
+    for i in range(numSegs):
+        cutXs = xs[20*i:20*i+20]
+        cutYs = ys[20*i:20*i+20]
+        A = fitLinear(cutXs,cutYs)
+        diff = np.absolute(np.subtract(cutYs, A[0]+cutXs*A[1]))
+        error = error + np.sum(diff)
+        #plt.plot(cutXs,A[0]+cutXs*A[1])
+    return error
+
+def calcPolyError(xs,ys,n):
+    numSegs = len(xs) // 20
+    error = 0
+    for i in range(numSegs):
+        cutXs = xs[20*i:20*i+20]
+        cutYs = ys[20*i:20*i+20]
+        A = fitPoly(cutXs,cutYs,n)
+        calcYs = addPolyTerms(cutXs,n) @ A  
+        diff = np.absolute(np.subtract(cutYs, calcYs))
+        error = error + np.sum(diff)
+        #plt.plot(cutXs,calcYs)
+    return error
+
+
+def calcTrigError(xs,ys):
+    numSegs = len(xs) // 20
+    error = 0
+    for i in range(numSegs):
+        cutXs = xs[20*i:20*i+20]
+        cutYs = ys[20*i:20*i+20]
+        A = fitTrig(cutXs,cutYs)
+        calcYs = addTrigTerms(cutXs) @ A  
+        diff = np.absolute(np.subtract(cutYs, calcYs))
+        error = error + np.sum(diff)
+        plt.plot(cutXs,calcYs)
+    return error
+
+print(calcLinearError(xs,ys))
+print(calcTrigError(xs,ys))
+for i in range(1,5):
+    print(calcPolyError(xs,ys,i))
 view_data_segments(xs,ys)
 
-
-x = np.array(xs)
-y = np.array(ys)
-print(x,y)
