d76b10747a30e96a1cd7e6decab5610e7647baa3
Jonathan Marriott <marriottjonny@gmail.com>
2021-04-05 21:59:41 +0100

    Tidy

--- lsr.py
+++ lsr.py
@@ -37,14 +37,12 @@ xs,ys = load_points_from_file(sys.argv[1])
 plot = False
 try:
     if sys.argv.index("--plot") > 0:
-        print("plotting")
         plot = True
 except:
     plot = False
 
 xs = np.array(xs)
 ys = np.array(ys)
-
 
 def mleFit(x,y):
     return np.linalg.inv(x.T.dot(x)).dot(x.T).dot(y)
@@ -57,7 +55,6 @@ def addPolyTerms(x,n):
     for i in range(2,n):
         xs = np.column_stack((xs,x**(i)))
     return xs
-
 
 def addTrigTerms(x):
     return np.column_stack((np.ones(x.shape),np.sin(x),np.cos(x)))
@@ -72,7 +69,6 @@ def fitPoly(xs,ys,n):
     return mleFit(addPolyTerms(xs,n),ys)
 
 def testTrain(xs,ys,testIndex):
-    #trainIndex = random.sample(range(20),15)
     xtrainVals = np.delete(xs,testIndex)
     xtestVals =  xs[testIndex]
     ytrainVals = np.delete(ys,testIndex)
@@ -84,10 +80,6 @@ def calcLinearError(trainXs, testXs, trainYs, testYs,doPlot=False):
     calcYs = addBias(testXs) @ A  
     diff = np.square(np.subtract(testYs, calcYs))
     error = np.sum(diff)
-    # plt.vlines(testXs,testYs,calcYs,colors='black')
-    # plt.plot(xs,addBias(xs) @ A,c='red')
-    # plt.scatter(trainXs,trainYs)
-    # plt.scatter(testXs,testYs,marker='x',c='orange')
     if doPlot:
         plt.plot(testXs,calcYs)
     return error
@@ -95,64 +87,21 @@ def calcLinearError(trainXs, testXs, trainYs, testYs,doPlot=False):
 def calcPolyError(trainXs, testXs, trainYs, testYs,n=3,doPlot=False):
     A = fitPoly(trainXs,trainYs,n)
     calcYs = addPolyTerms(testXs,n) @ A  
-    #plt.vlines(testXs,testYs,calcYs,colors='black')
     diff = np.square(np.subtract(testYs, calcYs))
     error = np.sum(diff)
-   # plt.plot(xs,addTrigTerms(xs) @ A,c='red')
-    #plt.scatter(cutXs,cutYs)
-    #plt.scatter(testXs,testYs,marker='x',c='orange')
     if doPlot:
         plt.plot(testXs,calcYs)
     return error
 
 
 def calcTrigError(trainXs, testXs, trainYs, testYs, doPlot=False):
-    #trainXs, testXs, trainYs, testYs = testTrain(xs,ys)
     A = fitTrig(trainXs,trainYs)
     calcYs = addTrigTerms(testXs) @ A  
-    #plt.vlines(testXs,testYs,calcYs,colors='black')
     diff = np.square(np.subtract(testYs, calcYs))
     error = np.sum(diff)
-   # plt.plot(xs,addTrigTerms(xs) @ A,c='red')
-    #plt.scatter(cutXs,cutYs)
-    #plt.scatter(testXs,testYs,marker='x',c='orange')
     if doPlot:
         plt.plot(testXs,calcYs)
     return error
-
-
-def plotLinear(xs,ys):
-    A = fitLinear(xs,ys)
-    calcYs = addBias(xs) @ A
-    plt.plot(xs,calcYs)
-    diff = np.square(np.subtract(ys, calcYs))
-    return np.sum(diff)
-
-def plotPoly(xs,ys,n=3):
-    A = fitPoly(xs,ys,n)
-    calcYs = addPolyTerms(xs,n) @ A
-    plt.plot(xs,calcYs)
-    diff = np.square(np.subtract(ys, calcYs))
-    return np.sum(diff)
-
-def plotTrig(xs,ys):
-    A = fitTrig(xs,ys)
-    calcYs = addTrigTerms(xs) @ A
-    plt.plot(xs,calcYs)
-    diff = np.square(np.subtract(ys, calcYs))
-    return np.sum(diff)
-# print(calcLinearError(xs,ys))
-# print(calcTrigError(xs,ys))
-# for i in range(1,5):
-#     print(calcPolyError(xs,ys,i))
-#
-
-# numSegs = len(xs) // 20
-# for i in range(numSegs):
-#         cutXs = xs[20*i:20*i+20]
-#         cutYs = ys[20*i:20*i+20]
-#         print(calcTrigError(cutXs,cutYs))
-# plt.show()
 
 def meanError(func,xs,ys):
     return np.average([func(*testTrain(xs,ys,i)) for i in range(20)])
@@ -160,7 +109,7 @@ def meanError(func,xs,ys):
 
 numSegs = len(xs) // 20
 reconstructionError = 0
-weightsDict = {0:calcLinearError, 1:calcPolyError, 2:calcTrigError}
+weightsDict = [calcLinearError, calcPolyError, calcTrigError]
 for i in range(numSegs):
         cutXs = xs[20*i:20*i+20]
         cutYs = ys[20*i:20*i+20]
@@ -168,11 +117,11 @@ for i in range(numSegs):
         print(errors)
         
         best = np.argmin(errors)
-        print(f'Seg Num: {i} has best {best}')
+        print(f'Seg Num: {i} has best {["linear","poly","trig"][best]}')
         
-        #print(weightsDict[best](cutXs,cutYs))
         reconstructionError += weightsDict[best](cutXs,cutXs,cutYs,cutYs,plot)
-        
+
+print(reconstructionError)    
 if plot:
     view_data_segments(xs,ys)    
-print(reconstructionError)
+
